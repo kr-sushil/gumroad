699c9e41b8b3aed24ce108e318d289b1f1ad051e
Sushil Kumar <skdatyal@gmail.com>
2025-10-15 08:31:49 +0530

    update

--- app/javascript/components/server-components/Profile/PostPage.tsx
+++ app/javascript/components/server-components/Profile/PostPage.tsx
@@ -73,12 +73,12 @@ const PostPage = ({
         <h1>{subject}</h1>
         <time>{publishedAtFormatted}</time>
       </header>
-      <article className="border-border grid gap-8 border-b p-4 pt-8 text-lg lg:pt-12 lg:pb-8 lg:*:max-w-4xl">
+      <article className="border-border grid gap-8 border-b p-4 pt-8 text-lg lg:pt-12 lg:pb-8">
         {pageLoaded ? null : <LoadingSpinner width="2em" />}
-        <EditorContent className="rich-text" editor={editor} />
+        <EditorContent className="rich-text lg:max-w-4xl" editor={editor} />
 
         {call_to_action || download_url ? (
-          <div className="mb-4 grid">
+          <div className="mb-4 grid lg:max-w-4xl">
             {call_to_action ? (
               <p>
                 <a
